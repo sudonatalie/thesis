8a4efa964f317bbad4c2013621bf686c1935a4ad
Natalie Perna <nmperna@gmail.com>
2017-05-20 16:07:40 -0400

    Update CaseSquash.hs.

--- Figures/Haskell/CaseSquash.hs
+++ Figures/Haskell/CaseSquash.hs
@@ -16,16 +16,15 @@ import Agda.Utils.Impossible
 -- | Eliminates case expressions where the scrutinee has already
 -- been matched on by an enclosing parent case expression.
 squashCases :: QName -> TTerm -> TCM TTerm
-squashCases q body = return $ dedupTerm (0, []) body
+squashCases q body = return $ dedupTerm [] body
 
 -- | Case scrutinee (de Bruijn index) with alternative match
 --   for that expression, made up of qualified name of constructor
 --   and a list of its arguments (also as de Bruijn indices)
 type CaseMatch = (Int, (QName, [Int]))
 
--- | Environment containing next de Bruijn index to be introduced
---   and 'CaseMatch'es in scope.
-type Env = (Int, [CaseMatch])
+-- | Environment containing 'CaseMatch'es in scope.
+type Env = [CaseMatch]
 
 -- | Recurse through 'TTerm's, accumulting environment of case alternatives
 --   matched and replacing repeated cases.
@@ -33,19 +32,16 @@ type Env = (Int, [CaseMatch])
 --   terms are traversed.
 dedupTerm :: Env -> TTerm -> TTerm
 -- Increment indices in scope to account for newly bound variable
-dedupTerm env (TLam tt) = TLam (dedupTerm (shiftIndices (+1) env) tt)
-dedupTerm env (TLet tt1 tt2) = TLet (dedupTerm env tt1) (dedupTerm (shiftIndices (+1) env) tt2)
-
+dedupTerm env (TLam tt) = TLam (dedupTerm (shiftIndices (+1) <$> env) tt)
+dedupTerm env (TLet tt1 tt2) = TLet (dedupTerm env tt1) (dedupTerm (shiftIndices (+1) <$> env) tt2)
 -- Check if scrutinee is already in scope
-dedupTerm env@(n, cs) body@(TCase sc t def alts) = case lookup sc cs of
+dedupTerm env body@(TCase sc t def alts) = case lookup sc env of
   -- If in scope with match then substitute body
   Just match -> caseReplacement match body
-
   -- Otherwise add to scope in alt branches
   Nothing -> TCase sc t
     (dedupTerm env def)
     (map (dedupAlt sc env) alts)
-
 -- Continue traversing nested terms in applications
 dedupTerm env (TApp tt args) = TApp (dedupTerm env tt) (map (dedupTerm env) args)
 dedupTerm env body = body
@@ -67,22 +63,15 @@ lookupTACon _ [] = Nothing
 -- | Introduce new constructor matches into environment scope
 dedupAlt :: Int -> Env -> TAlt -> TAlt
 dedupAlt sc env (TACon name ar body) =
-  let env' = addToEnv (sc + ar, (name, [ar-1,ar-2..0])) (shiftIndices (+ar) env)
+  let env' = (sc + ar, (name, [ar-1,ar-2..0])):(shiftIndices (+ar) <$> env)
   in TACon name ar (dedupTerm env' body)
 dedupAlt sc env (TAGuard guard body) = TAGuard guard (dedupTerm env body)
 dedupAlt sc env (TALit lit body) = TALit lit (dedupTerm env body)
 
--- | Add a new case match to environment
-addToEnv :: CaseMatch -> Env -> Env
-addToEnv c (n, cs) = (n, c:cs)
-
--- | Shift all de Bruijn indices in an environment according to provided
+-- | Shift all de Bruijn indices in a case match according to provided
 --   function on integers
-shiftIndices :: (Int -> Int) -> Env -> Env
-shiftIndices f (n, cs) = (f n, map (shiftIndices' f) cs)
-  where
-    shiftIndices' :: (Int -> Int) -> CaseMatch -> CaseMatch
-    shiftIndices' f (sc, (name, vars)) = (f sc, (name, map f vars))
+shiftIndices :: (Int -> Int) -> CaseMatch -> CaseMatch
+shiftIndices f (sc, (name, vars)) = (f sc, (name, map f vars))
 
 -- | Substitute list of current de Bruijn indices for list of new indices
 --   in a term
